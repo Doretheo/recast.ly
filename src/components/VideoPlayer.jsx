3878358b4a1decd5f41132ce2781a82ae64dfa16
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-09-01 17:22:55 -0700

    Passed a few tests

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,11 @@
-var VideoPlayer = () => (
+var VideoPlayer = ({video}) => (
+  // console.log(props),
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
+      <h3></h3>
       <div>Video Description</div>
     </div>
   </div>
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,33 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // console.log(key);
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      key: options.key,
+      q: options.query,
+      part: 'snippet',
+      maxResults: options.max,
+      type: 'video'
+    },
+
+    videoEmbeddable: true,
+    success: (({data}) => {
+      if (callback) {
+        callback(data);
+        console.log('Success!', data);
+      }
+    }),
+
+    error: (({data}) => {
+      console.log('Couldn\'t get data', data);
+    })
+  });
 };
 
+// used to export a single class, function or primitive from a script file
 export default searchYouTube;
+
+// data comes back before finishes loading
+// if componenet finishes mouting first, more stable outcome
+// search youtube belongs in a mounting div
